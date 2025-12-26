b10a0182099bb7abb3b324e069df63a2f07242c2
diok4 <diokworkaet@gmail.com>
2025-12-27 01:17:33 +0500

    updated: widgets/navbar ui

--- src/widgets/navbar/index.tsx
+++ src/widgets/navbar/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import { RxExit } from "react-icons/rx";
 import { FaCircleUser } from "react-icons/fa6";
 import { MdChat } from "react-icons/md";
 import { IoIosSettings } from "react-icons/io";
 import { NavLink } from "@/src/widgets/shared/navlink";
 import { logout } from "@/src/features/auth/logout";
+import { FaArrowDownWideShort } from "react-icons/fa6";
 
 interface NavBarProps {
   user: any;
@@ -14,21 +14,21 @@ interface NavBarProps {
 
 export const NavBar = ({ user }: NavBarProps) => {
   return (
-    <div className="w-[250px] h-screen py-3 px-5 border-gray-200 border-r flex flex-col justify-between">
+    <div className="w-[250px] h-screen py-5 px-5 border-gray-200 border-r flex flex-col justify-between">
       <div>
-        <div className="flex items-center pb-2">
-          <Image src="/logo.png" alt="Logo" width={50} height={50} />
+        <div className="flex items-center pb-2 font-extrabold bg-linear-to-r from-blue-800 via-blue-950 to-blue-900 bg-clip-text text-transparent  text-3xl select-none">
+          <FaArrowDownWideShort className="text-blue-900" />
           <h1 className="ml-2 font-medium">Shortify</h1>
         </div>
 
         {user ? (
-          <div className="flex flex-col items-center border-t border-gray-200 pt-2">
+          <div className="flex flex-col items-center border-t border-gray-200 pt-5">
             <img src={user.avatarUrl} width={50} height={50} alt="404" />
             <h2 className="text-lg font-normal capitalize">{user.username}</h2>
             <p className="text-gray-700 text-sm">{user.email}</p>
           </div>
         ) : (
-          <div className="flex flex-col items-center border-t border-gray-200 pt-2">
+          <div className="flex flex-col items-center border-t border-gray-200 pt-5">
             <FaCircleUser className="text-[60px] text-gray-400" />
             <p className="text-gray-500 text-sm mt-1">Guest</p>
           </div>
